4423cfd849ba592f56fedfb2393c57d70558248a
Manuelgruiz <manuelgruiz22@gmail.com>
2024-04-30 18:06:14 +0200

    Add superuser

--- src/main/java/com/tribeadsapi/tribeads/config/SecurityConfig.java
+++ src/main/java/com/tribeadsapi/tribeads/config/SecurityConfig.java
@@ -1,15 +1,38 @@
 package com.tribeadsapi.tribeads.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${spring.application.user}")
+    private String usuario;
+
+    @Value("${spring.application.password}")
+    private String contrasena;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username(usuario)
+                .password(passwordEncoder().encode(contrasena))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
